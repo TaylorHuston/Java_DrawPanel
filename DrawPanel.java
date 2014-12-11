5e2936416a08bb173af9c36abbf7d8bd66a74de9
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-10 23:25:42 -0700

    Fix formatting based on checkstyle

--- DrawPanel.java
+++ DrawPanel.java
@@ -1,4 +1,3 @@
-
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,32 +6,30 @@ import javax.swing.JPanel;
  * Simple program to draw some lines
  * From "Java How To Program, 10/e, Early Objects" - Chapter 4
  */
-
-
-
 public class DrawPanel extends JPanel {
 
     //Draws an X from the corners of the panel
     public void paintComponent(Graphics g) {
         
-        super.paintComponent(g); //Call paintCompnenet to ensure the panel displays correctly
+        //Call paintCompnenet to ensure the panel displays correctly
+        super.paintComponent(g);
         
         int width = getWidth();
         int height = getHeight();
         
-        g.drawLine(0, 0, width,height);
-        g.drawLine(0,height,width, 0);
+        g.drawLine(0, 0, width, height);
+        g.drawLine(0, height, width, 0);
         
-    }//End class paintComponent
+    } //End paintComponent
     
+    //Test for DrawPanel
     public static void main(String[] args) {
-        
         DrawPanel myPanel = new DrawPanel(); //Create panel to hold drawing
-        JFrame app = new JFrame();  //Create fram to hold panel
+        JFrame app = new JFrame();  //Create frame to hold panel
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        app.add(myPanel); // add the panel to the frame      
-        app.setSize(250, 250); // set the size of the frame 
-        app.setVisible(true); // make the frame visible
-    }//End main
+        app.add(myPanel); //Add the panel to the frame      
+        app.setSize(250, 250); //Set the size of the frame 
+        app.setVisible(true); //Make the frame visible
+    } //End main
     
-}//End class DrawPanel
+} //End DrawPanel
